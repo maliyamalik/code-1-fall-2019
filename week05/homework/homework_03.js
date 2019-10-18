78ffd4590ef0c7fba60d23efbe44cd1bb8d3e938
maliyamalik <56562535+maliyamalik@users.noreply.github.com>
2019-10-17 22:05:21 -0400

    Update homework_03.js

--- week05/homework/homework_03.js
+++ week05/homework/homework_03.js
@@ -7,12 +7,16 @@ function draw() {
     rectMode(CENTER);
     fill("#EE3366");
     // a coordinate-related function needs to be used here
+    resetMatrix();
     angleMode(DEGREES);
     // a coordinate-related function needs to be used here
+    rotate(PI / 0.002);
     // a coordinate-related function needs to be used here
+    translate(-130, -205);
     rect(0, 0, 50, 50);
     rect(100, 0, 50, 50);
     rect(200, 0, 50, 50);
     rect(300, 0, 50, 50);
     // a coordinate-related function needs to be used here
+    
 }
